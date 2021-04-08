ae3fa4dd9e833be49d0db02ef4898ff63fe7c53a
Mohammad Ishraq Kabir <ishraqkabir@pop-os.localdomain>
2021-04-08 13:25:16 +0600

    link hardcoded

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3d83b37520d8aa64d9fa";
+/******/ 	var hotCurrentHash = "686b2157424af3d667d7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
